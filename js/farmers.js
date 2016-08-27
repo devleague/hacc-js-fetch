bc8e88d2cda4aa30d906d37106ebf99f270a7e12
Jon Borgonia <jon@gomagames.com>
2016-08-27 12:28:05 -1000

    render some data with vanilla dom

--- js/farmers.js
+++ js/farmers.js
@@ -5,6 +5,44 @@
     return Object.keys(params).map( key => `${key}=${encodeURIComponent(params[key])}`).join('&');
   }
 
+  /*
+   * FarmersMarket
+   * contact :"Paul Richard"
+   * farmer_s_market : "Country Market & Craft Fair at Waimanalo Homestead Community Center"
+   * island : "Oahu"
+   * location_1_location : "1330 Kalanianaole Hwy."
+   * location_1_state : "HI"
+   * location_info : "Waimanalo Homestead Community Center"
+   * phone : "(808) 259-8611, Cell: (808) 284-2575"
+   * sunday : "X"
+   * time : "9:00 AM to 4:00PM"
+   */
+  function farmersRender( farmers ){
+    // append each farmer to the dom
+    var container = document.getElementById('farmers');
+
+    farmers.map(function(market){
+      var child = document.createElement('div');
+      child.classList.add('farmersMarket');
+
+      var contact = document.createElement('h3');
+      contact.innerHTML = market.contact || '(Missing Contact)';
+
+      var farmer_s_market = document.createElement('h4');
+      farmer_s_market.innerHTML = market.farmer_s_market;
+
+      var location = document.createElement('p');
+      location.innerHTML = market.location_info || '(Missing location)';
+
+      child.appendChild(contact);
+      child.appendChild(farmer_s_market);
+      child.appendChild(location);
+      return child;
+    })
+    .forEach(container.appendChild.bind(container));
+
+  }
+
   var farmersMarketEndpoint = 'https://data.hawaii.gov/resource/b2y9-ab7v.json';
 
   var queryParams = {
@@ -17,6 +55,6 @@
     .then(function(response) {
       return response.json();
     })
-    .then(console.log);
+    .then(farmersRender);
 
 })();
