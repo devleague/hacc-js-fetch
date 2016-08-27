c7700204e19e56db9cafc6caabdbbdc2d3f9f856
Jon Borgonia <jon@gomagames.com>
2016-08-27 10:43:23 -1000

    add queryParams for  to fetch 50 farmers

--- js/farmers.js
+++ js/farmers.js
@@ -1,7 +1,19 @@
 (function(){
+
+  // custom encoding function, because querystring.encode will encode the $ => %24
+  function qsEncode( params ){
+    return Object.keys(params).map( key => `${key}=${encodeURIComponent(params[key])}`).join('&');
+  }
+
   var farmersMarketEndpoint = 'https://data.hawaii.gov/resource/b2y9-ab7v.json';
 
-  fetch(farmersMarketEndpoint)
+  var queryParams = {
+    $limit : 50
+  };
+
+  var farmersRequest = new Request(farmersMarketEndpoint + "?" + qsEncode(queryParams));
+
+  fetch(farmersRequest)
     .then(function(response) {
       return response.json();
     })
