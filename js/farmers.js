270ac9a1a1547f6170146352135774881a57106b
Jon Borgonia <jon@gomagames.com>
2016-08-27 10:27:08 -1000

    use fetch to get json

--- js/farmers.js
+++ js/farmers.js
@@ -1,5 +1,10 @@
 (function(){
   var farmersMarketEndpoint = 'https://data.hawaii.gov/resource/b2y9-ab7v.json';
 
-  console.log(farmersMarketEndpoint);
+  fetch(farmersMarketEndpoint)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(console.log);
+
 })();
